81a10b54e4ae08f20ce7d7b1d383c6e34f66576c
vanphuoc9 <thaiphuoc1997@gmail.com>
2018-09-29 11:17:08 +0700

    second

--- train.py
+++ train.py
@@ -9,13 +9,11 @@ import matplotlib.pyplot as plt
 import random
 import numpy as np
 import time
-train = pd.read_csv('train.csv')
+train = pd.read_csv('traintest.csv')
 data_dir = os.getcwd()
 def parser(row):
    # function to load files and extract features
    file_name = os.path.join(os.path.abspath(data_dir), 'audio_train', str(row.fname))
-   print (file_name)
-
    # handle exception to check if there isn't a file which is corrupted
    try:
        X, sample_rate = librosa.load(file_name, res_type='kaiser_fast')
@@ -25,18 +23,25 @@ def parser(row):
    except Exception as e:
       print("Error encountered while parsing file: ", file)
       return None, None
+
    feature = mfccs
    label = row.label
-   return [feature, label]
+   print (file_name)
+   return pd.Series([feature, label])
 
 temp = train.apply(parser, axis=1)
 temp.columns = ['feature', 'label']
-print (temp.head(2))
+# print(temp.head(2))
+# print(temp.feature.tolist())
 
 #Convert the data to pass it in our deep learning model
 from sklearn.preprocessing import LabelEncoder
+# # # from keras.utils import np_utils
 X = np.array(temp.feature.tolist())
 y = np.array(temp.label.tolist())
+# #
+print (X)
 
 lb = LabelEncoder()
-y = np_utils.to_categorical(lb.fit_transform(y))
+print(lb.fit_transform(y)[:5])
+# y = np_utils.to_categorical(lb.fit_transform(y))
